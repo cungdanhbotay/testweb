0af02face4a5dfbf96815ef86c0d10a7a1bee3f8
baophan1223 <cungdanhbotay@gmail.com>
2021-11-04 16:18:51 +0700

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,5 +1,5 @@
 <?php
-    $conn = pg_connect("postgres://pksfjeakmhhvga:d399c1e2e761dbec8269fed1278dd57061d8ada8b45dcd8c429df771e114c609@ec2-44-198-215-235.compute-1.amazonaws.com:5432/dcoboa1dd6vgou");
+    $conn = pg_connect("postgres://epcxrxdunczekr:01564fdcc92469ec4a4d6c5e346c2a0c9e817625dd6548d832a2fca0536f74ea@ec2-3-215-57-87.compute-1.amazonaws.com:5432/d9n65ptr1efl6j");
 
     if(!$conn){
         die("Can not connect database");
